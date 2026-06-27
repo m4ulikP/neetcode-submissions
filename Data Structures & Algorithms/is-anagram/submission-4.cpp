400f4fe5a36da217114ca20705fbc01610aa7ee2
Maulik Pandey <161493361+m4ulikP@users.noreply.github.com>
2026-06-27 17:19:41 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        unordered_map<char, int> str1;
+        unordered_map<char, int> str2;
+        for (char c : s) str1[c]++;
+        for (char c : t) str2[c]++;
+        for (char c : s){
+            if (str1[c] != str2[c]){
+                return false;}
+        }
+        return true;
+    }
+};
